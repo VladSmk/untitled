182131df97aef216819c2984bde709eaacb8b446
vladyslave <svlad2331@gmail.com>
2024-07-27 18:54:54 +0300

    Commit changes 54

--- src/components/Explosive.js
+++ src/components/Explosive.js
@@ -3,10 +3,7 @@ import '../assets/styles/Explosive.css';
 
 const Explosive = ({ name, image, onClick, selected }) => {
     return (
-        <div
-            className={`explosive-item ${selected ? 'selected' : ''}`}
-            onClick={onClick}
-        >
+        <div className={`explosive-item ${selected ? 'selected' : ''}`} onClick={onClick}>
             <img src={image} alt={name} />
             <div className="explosive-name">{name}</div>
         </div>
