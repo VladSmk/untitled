124e7e06be5c267fb2a8c0b05795c809c1e71b02
vladyslave <svlad2331@gmail.com>
2024-07-27 15:00:27 +0300

    Commit changes 18

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Check from "./pages/Check";
 import Final from "./pages/Final";
 import './assets/styles/App.css';
 
-// const tele = window.Telegram.WebApp;
-// tele.expand();
+const tele = window.Telegram.WebApp;
+tele.expand();
 
 function App() {
 
